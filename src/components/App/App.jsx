0ab1511e4db36fcd317c4ed9cc1a336a9485798d
Vladyslav Tykhyi <vladleonidovich01@gmail.com>
2025-12-25 09:42:59 +0100

    naming again

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import Feedbacks from "../Feedbacks/Feedbacks";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
-import Hero from "../hero/hero";
+import Hero from "../Hero/Hero";
 import Location from "../Location/Location";
 import s from "./App.module.css";
 
